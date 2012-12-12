f2c5bc84bbdee2529f3bdaef47920843caf618c9
gogromat <gogromat@gmail.com>
2012-12-11 22:02:07 -0500

    Convert to rails

---
+++ joinme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joinme::Application.config.secret_token = 'bed6df1b23e6f258119aeda9fb6b9682eea1da0eb2145216fa601e955a7f38104b97b3d59a04daa36e58213dfcaf02577585e7c6283e3e59ee3f651bc97bb892'
